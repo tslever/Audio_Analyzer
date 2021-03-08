75fed6656404ef9f24ff9b3416fa17a6f2a1fc9c
Tom Lever <thomas.lever.business@gmail.com>
2021-03-08 14:35:31 -0500

    Table of NOTE_ON's, NOTE_OFF's
    Simplified Main.java so that method main outputs a table where each row corresponds to a NOTE_ON or NOTE_OFF ShortMessage and contains tick, channel, command, note, and velocity.

--- midireader/src/main/java/com/mycompany/midireader/Main.java
+++ midireader/src/main/java/com/mycompany/midireader/Main.java
@@ -24,12 +24,6 @@ import javax.sound.midi.Track;
  */
 public class Main {
     
-    /*private static final float PPQ = 0.0f;
-    private static final float SMPTE_24 = 24.0f;
-    private static final float SMPTE_25 = 25.0f;
-    private static final float SMPTE_30 = 30.0f;
-    private static final float SMPTE_30DROP = 29.97f;*/
-    
     private static final int NOTE_ON = 0x90;
     private static final int NOTE_OFF = 0x80;
     public static final String[] NOTE_NAMES =
@@ -38,8 +32,7 @@ public class Main {
     public static void main(String[] args)
         throws NotOneInputException,
                InvalidMidiDataException,
-               IOException,
-               FunctionalityNotImplementedException {
+               IOException {
         
         InputParsingUtilities.checkNumberOfInputsIn(args);
         // throws NotOneInputException
@@ -48,91 +41,60 @@ public class Main {
         File file = new File(filename);
         Sequence sequence = MidiSystem.getSequence(file);
         // throws InvalidMidiDataException, IOException
-        /*if (!(sequence.getDivisionType() == PPQ)) {
-            throw new FunctionalityNotImplementedException(
-                "No functionality implemented to handle SMPTE division type.");
-        }*/
         System.out.println(
             "Sequence sequence" + "\n" +
-            "Length of sequence in ticks: " + sequence.getTickLength() + "\n" +
             "Length of sequence in microseconds: " +
-            sequence.getMicrosecondLength());
+            sequence.getMicrosecondLength() + "\n" +
+            "Length of sequence in ticks: " + sequence.getTickLength() + "\n");
         
-        int trackNumber = 0;
-        for (Track track : sequence.getTracks()) {
-            
-            trackNumber++;
-            System.out.println(
-                "-Ss-Track " + trackNumber + "\n" +
-                "-Ss-Length of track in ticks: " + track.ticks() + "\n" +
-                "-Ss-Number of MidiEvents in track: " + track.size());
+        for (Track track : sequence.getTracks()) {    
             
             for (int i = 0; i < track.size(); i++) {
                 
                 MidiEvent midiEvent = track.get(i);
-                System.out.println(
-                    "-Ss--T" + trackNumber + "-" +
-                    "MidiEvent " + i + "\n" +
-                    "-Ss--T" + trackNumber + "-" +
-                    "Tick of MidiEvent: " + midiEvent.getTick());
                 
                 MidiMessage midiMessage = midiEvent.getMessage();
+                
                 if (midiMessage instanceof ShortMessage) {
+                    
                     ShortMessage shortMessage = (ShortMessage)midiMessage;
-                    System.out.println(
-                        "-Ss--T" + trackNumber + "-" +
-                        "Class of MidiMessage of MidiEvent: ShortMessage" + "\n" +
-                        "-Ss--T" + trackNumber + "-" +
-                        "MIDI channel of ShortMessage of MidiEvent: " +
-                        shortMessage.getChannel());
+                    
                     if (shortMessage.getCommand() == NOTE_ON) {
-                        String command = "NOTE_ON";
+                        
+                        int pianoKey = shortMessage.getData1();
+                        int note = pianoKey % 12;
+                        String noteName = NOTE_NAMES[note];
+                        int octave = (pianoKey / 12) - 1;                        
+                        int velocity = shortMessage.getData2();
+                        
+                        System.out.println(
+                            "Tick: " + midiEvent.getTick() + " | " +
+                            "Channel: " + shortMessage.getChannel() + " | " +
+                            "Command: NOTE_ON | " +
+                            "Note: " + noteName + octave + " | " +
+                            "Velocity: " + velocity);
+                    }
+                    else if (shortMessage.getCommand() == NOTE_OFF) {
+                        
                         int pianoKey = shortMessage.getData1();
                         int note = pianoKey % 12;
                         String noteName = NOTE_NAMES[note];
                         int octave = (pianoKey / 12) - 1;
                         int velocity = shortMessage.getData2();
+                        
                         System.out.println(
-                            "-Ss--T" + trackNumber + "-" +
-                            "Command of ShortMessage of MidiEvent: " + command + "\n" +
-                            "-Ss--T" + trackNumber + "-" +
-                            "Pitch of ShortMessage of MidiEvent: " + noteName + octave + "\n" +
-                            "-Ss--T" + trackNumber + "-" +
-                            "Velocity of ShortMessage of MidiEvent: " + velocity);
+                            "Tick: " + midiEvent.getTick() + " | " +
+                            "Channel: " + shortMessage.getChannel() + " | " +
+                            "Command: NOTE_OFF | " +
+                            "Note: " + noteName + octave + " | " +
+                            "Velocity: " + velocity);
                     }
-                    else if (shortMessage.getCommand() == NOTE_ON) {
-                        String command = "NOTE_OFF";
-                        int pianoKey = shortMessage.getData1();
-                        int note = pianoKey % 12;
-                        String noteName = NOTE_NAMES[note];
-                        int octave = (pianoKey / 12) - 1;
-                        int velocity = shortMessage.getData2();
-                        System.out.println(
-                            "-Ss--T" + trackNumber + "-" +
-                            "Command of ShortMessage of MidiEvent: " + command + "\n" +
-                            "-Ss--T" + trackNumber + "-" +
-                            "Pitch of ShortMessage of MidiEvent: " + noteName + octave + "\n" +
-                            "-Ss--T" + trackNumber + "-" +
-                            "Velocity of ShortMessage of MidiEvent: " + velocity);
-                    }
-                    else {
-                        System.out.println(
-                            "-Ss--T" + trackNumber + "-" +
-                            "Command of ShortMessage of MidiEvent: " + shortMessage.getCommand());
-                    }
+                    else { }
+                    // shortMessage.getCommand() does not equal NOTE_ON.
+                    // shortMessage.getCommand() does not equal NOTE_OFF.
                 }
-                else if (midiMessage instanceof MetaMessage) {
-                    MetaMessage metaMessage = (MetaMessage)midiMessage;
-                    System.out.println(
-                        "-Ss--T" + trackNumber + "-" +
-                        "Class of MidiMessage of MidiEvent: MetaMessage");
-                    //metaMessage.
-                }
-                else {
-                    System.out.println(
-                        "-Ss--T" + trackNumber + "-" +
-                        "Class of MidiMessage of MidiEvent: " + midiMessage.getClass());
-                }
+                else { }
+                // midiMessage.getClass() does not result in ShortMessage.
             }
         }
     }
