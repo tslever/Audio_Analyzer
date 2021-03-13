1aefc5311edcb6297ad1ae2e46188d682464f452
Tom Lever <thomas.lever.business@gmail.com>
2021-03-12 23:00:39 -0500

    TreeMap of Channels and ArrayLists of Instances of Tick, Command, Key, and Velocity
    Going to work on, for each channel, determining which note is playing at a given time.

--- eclipse-workspace/midi-to-wav-converter/src/converter_utilities/Main.java
+++ eclipse-workspace/midi-to-wav-converter/src/converter_utilities/Main.java
@@ -1,15 +1,43 @@
 package converter_utilities;
 
 
+import java.io.File;
+import java.io.IOException;
+import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import java.util.ArrayList;
+import java.util.TreeMap;
+import midi_parsing_utilities.MidiParser;
+import midi_parsing_utilities.TickCommandKeyAndVelocity;
 
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NotOneInputException, IOException, InvalidMidiDataException {
 
-		System.out.println("Hello World!");
+		InputParsingUtilities.checkNumberOfInputsIn(args); // throws NotOneInputException
+		
+		String filename = args[0];
+		File file = new File(filename);
+		Sequence sequence = MidiSystem.getSequence(file); // throws IOException, InvalidMidiDataException
+		
+		TreeMap<Integer, ArrayList<TickCommandKeyAndVelocity>> treeMapOfChannelsAndArrayLists =
+			MidiParser.getTreeMapOfChannelsAndArrayListsFrom(sequence);
+		
+		for (int channel : treeMapOfChannelsAndArrayLists.keySet()) {
+			for (TickCommandKeyAndVelocity tickCommandKeyAndVelocity : treeMapOfChannelsAndArrayLists.get(0)) {
+				System.out.println(
+					"Channel: " + channel + " | " +
+					"Tick: " + tickCommandKeyAndVelocity.getTick() + " | " +
+					"Command: " + tickCommandKeyAndVelocity.getCommand() + " | " +
+					"Key: " + tickCommandKeyAndVelocity.getKey() + " | " +
+					"Velocity: " + tickCommandKeyAndVelocity.getVelocity()
+				);
+			}	
+		}
 
 	}
+
 
 }
---
+++ eclipse-workspace/midi-to-wav-converter/src/midi_parsing_utilities/TickCommandKeyAndVelocity.java
@@ -0,0 +1,35 @@
+package midi_parsing_utilities;
+
+public class TickCommandKeyAndVelocity {
+
+	private int tick;
+	private int command;
+	private int key;
+	private int velocity;
+	
+	public TickCommandKeyAndVelocity(int tickToUse, int commandToUse, int keyToUse, int velocityToUse) {
+		
+		this.tick = tickToUse;
+		this.command = commandToUse;
+		this.key = keyToUse;
+		this.velocity = velocityToUse;
+		
+	}
+	
+	public int getTick() {
+		return this.tick;
+	}
+	
+	public int getCommand() {
+		return this.command;
+	}
+	
+	public int getKey() {
+		return this.key;
+	}
+	
+	public int getVelocity() {
+		return this.velocity;
+	}
+	
+}
